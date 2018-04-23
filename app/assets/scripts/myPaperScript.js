55ff7d871816c8386efb1d3c5bcd8e658acd87ea
Paul <pfreema1@gmail.com>
2018-04-23 13:56:34 -0400

    foo

--- app/assets/scripts/myPaperScript.js
+++ app/assets/scripts/myPaperScript.js
@@ -1,6 +1,34 @@
 console.log(project);
+/*****************************
+ **		background
+ ******************************/
+
+var rect = new Path.Rectangle({
+  point: [0, 0],
+  size: [view.size.width, view.size.height],
+  strokeColor: 'white',
+  selected: true
+});
+rect.sendToBack();
+rect.fillColor = 'blue';
+
+/*****************************
+ **		constants
+ ******************************/
+var BOTTOM_FRONT_CENTER_MIN_HEIGHT = view.size.height * 0.75;
+
+/*****************************
+ **		leftLine
+ ******************************/
 
 var leftLine = new Path({
   strokeWidth: 2,
   strokeColor: 'white'
 });
+leftLine.add(
+  new Point(view.center.x, BOTTOM_FRONT_CENTER_MIN_HEIGHT),
+  new Point(view.size.width * 0.25, BOTTOM_FRONT_CENTER_MIN_HEIGHT)
+);
+
+var leftLineGroup = new Group([leftLine]);
+leftLineGroup.rotate(30);
