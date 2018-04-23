7333ad8f9df9267f2334d019e7e5cc1d09513984
Paul <pfreema1@gmail.com>
2018-04-23 13:27:38 -0400

    wavy

--- app/assets/scripts/myPaperScript.js
+++ app/assets/scripts/myPaperScript.js
@@ -1,12 +1,46 @@
-var triangle = new Path.RegularPolygon(new Point(80, 70), 3, 50);
-triangle.fillColor = '#bada55';
-triangle.selected = true;
+console.log(project);
+/*****************************/
 
-var decagon = new Path.RegularPolygon(new Point(260, 70), 10, 50);
-decagon.fillColor = '#f00f00';
-decagon.selected = true;
-decagon.removeSegment(0);
-decagon.removeSegment(0);
-decagon.removeSegment(0);
-decagon.removeSegment(0);
-decagon.smooth();
+/*****************************
+ **		background
+ ******************************/
+
+var rect = new Path.Rectangle({
+  point: [0, 0],
+  size: [view.size.width, view.size.height],
+  strokeColor: 'white',
+  selected: true
+});
+rect.sendToBack();
+rect.fillColor = 'blue';
+
+/*****************************/
+
+var segmentAmount = 5;
+var height = 60;
+var lineLength = 300;
+var path = new Path({
+  strokeColor: 'white',
+  strokeWidth: 2
+});
+var group = new Group([path]);
+
+for (var i = 0; i <= segmentAmount; i++) {
+  path.add(new Point(i / segmentAmount, 1) * lineLength);
+}
+
+path.selected = true;
+group.position = view.center;
+group.transformContent = false;
+group.rotate(30);
+
+function onFrame(event) {
+  for (var i = 0; i <= segmentAmount; i++) {
+    var segment = path.segments[i];
+    var sinus = Math.sin(event.time * 3 + i);
+    segment.point.y = sinus * height + view.center.y;
+  }
+  path.smooth();
+}
+
+console.log(view.center);
